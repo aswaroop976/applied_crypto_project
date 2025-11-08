fb7340e4346fb32b155cd5fbe3b60a313b6a76e4
Arpan Swaroop <arpan.swaroop@gmail.com>
2025-11-08 13:13:47 -0500

    added requirements.txt

--- pdq_test.py
+++ pdq_test.py
@@ -1,10 +1,11 @@
 # tests/hash_check.py
 import cv2, pdqhash, imagehash
 from PIL import Image
-img = cv2.imread('/home/arpan/Documents/crypto/project/adversarial-detection-avoidance-attacks/datasets/n01440764_18.JPEG')
+img = cv2.imread('/home/arpan/Documents/crypto/project/src/images_in/Stevens_Alfred_Jeune.jpg')
 print(type(img))
 #img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 hv, q = pdqhash.compute(img)
 print("PDQ hash vector len:", len(hv), "quality:", q)
-p = imagehash.phash(Image.open('/home/arpan/Documents/crypto/project/adversarial-detection-avoidance-attacks/datasets/n01440764_18.JPEG'))
+p = imagehash.phash(Image.open('/home/arpan/Documents/crypto/project/src/images_in/Stevens_Alfred_Jeune.jpg'))
 print("pHash:", str(p))
+print(f"type of phash hash:{type(p)}")
